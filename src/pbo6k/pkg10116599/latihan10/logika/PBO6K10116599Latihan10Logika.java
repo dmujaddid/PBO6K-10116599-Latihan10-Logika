e9678cb2e07ff0b5c713c3cad7ee8a4255ea68da
dmujaddid <robyeka@email.unikom.ac.id>
2018-09-23 11:22:01 +0700

    first commit

---
+++ src/pbo6k/pkg10116599/latihan10/logika/PBO6K10116599Latihan10Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan10.logika;
+
+/**
+ *
+ * @author
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : Program penggunaan tipe data logika.
+ * 
+ */
+public class PBO6K10116599Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur>=17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
